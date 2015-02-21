451af4dd384eb23483613cf2e7e4764dbf2099d6
Pranjal Vachaspati <pranjal@pranj.al>
2015-02-20 21:34:01 -0600

    add script to initialize scratch folders

---
+++ taub_scripts/init_folders.sh
@@ -0,0 +1,11 @@
+# you should have in your basedir:
+#  - whatever methods you're using; e.g. the ASTRAL folder, the wQMC folder, etc.
+#  - the hgt-data folder (and whatever other data sets you're using)
+#
+# in principle you can put these wherever, but then you'll have to change some
+# things in generate_qsub_script.py
+
+mkdir ~/scratch/trees
+mkdir ~/scratch/branchrates
+mkdir ~/scratch/quartetscores
+mkdir ~/scratch/timings
