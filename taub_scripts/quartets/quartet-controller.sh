e75cbd5d0c2bb42bbd6d0a3d1d721487b82a71e1
Pranjal Vachaspati <pranjal@pranj.al>
2015-02-21 16:20:15 -0600

    fix quartet script location

--- taub_scripts/quartets/quartet-controller.sh
+++ taub_scripts/quartets/quartet-controller.sh
@@ -4,7 +4,7 @@ quartetcounts=${2}
 
 if [ ! -s $quartetcounts ]
 then
-    sh ~/Dropbox/Programs/phylogenetics/quartets/quartet_count.sh $1 | perl ~/Dropbox/Programs/phylogenetics/quartets/summarize_quartets_stdin.pl > $2
+    sh ~/phylogenetics/quartets/quartet_count.sh $1 | perl ~/phylogenetics/quartets/summarize_quartets_stdin.pl > $2
 fi
 
 
