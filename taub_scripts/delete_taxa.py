a6c52200a949e6e6c161f0d90a227879f2e6ac62
Vachaspati <vachasp2@taubh1.campuscluster.illinois.edu>
2015-03-25 14:53:23 -0500

    make deleting taxa faster

--- taub_scripts/delete_taxa.py
+++ taub_scripts/delete_taxa.py
@@ -11,12 +11,19 @@ if __name__ == '__main__':
         print "delete_taxa.py input ndelete output"
         exit()
     inp = sys.argv[1]
-    ndelete = int(sys.argv[2])
-    output = sys.argv[3]
+    ndelete = []
+    for i in sys.argv[2:]:
+        ndelete.append(int(i))
     
+    ndelete.sort()
+
     tl = dendropy.TreeList()
     tl.read_from_path(inp, 'newick')
-    
-    for t in tl:
-        delete_taxa(t, ndelete)
-    tl.write_to_path(output, 'newick')
+
+    pn = 0
+
+    for n in ndelete:
+        for t in tl:
+            delete_taxa(t, n - pn)
+        pn = n
+        tl.write_to_path(inp + "m" + "%02d" % n, 'newick')
