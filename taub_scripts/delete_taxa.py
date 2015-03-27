a003da490784be4c514a1125a056ca871ba54142
Pranjal Vachaspati <pranjal@pranj.al>
2015-03-27 17:31:15 -0500

    add sequences to delete_taxa

--- taub_scripts/delete_taxa.py
+++ taub_scripts/delete_taxa.py
@@ -1,30 +1,59 @@
 import dendropy
 import numpy as np
 
-def delete_taxa(t, n):
-    t.prune_taxa([i.taxon for i in np.random.choice(t.leaf_nodes(), size=n)])
+def delete_taxa(t, seqsubset, n):
+    taxa = [i.taxon for i in np.random.choice(t.leaf_nodes(), size=n)]
+    labels = set([str(i.label) for i in taxa])
+    t.prune_taxa(taxa)
+    seqsubset =[i for i in seqsubset if i.split()[0] not in labels]
+    return seqsubset
     
+    
+def isboundary(s):
+    splits = s.split()
+    if len(splits) > 1:
+        return splits[1].isdigit()
+    return False
 
 import sys
 if __name__ == '__main__':
     if len(sys.argv) < 4:
-        print "delete_taxa.py input ndelete..."
+        print "delete_taxa.py input seq_input ndelete..."
         exit()
     inp = sys.argv[1]
+    seq_inp = sys.argv[2]
     ndelete = []
-    for i in sys.argv[2:]:
+    for i in sys.argv[3:]:
         ndelete.append(int(i))
     
     ndelete.sort()
-
+    print inp
+    print seq_inp
+    print ndelete
     tl = dendropy.TreeList()
     tl.read_from_path(inp, 'newick')
-    tl.append(dendropy.treesim.star_tree(tl.taxon_set))
+    startree = dendropy.treesim.star_tree(tl.taxon_set)
+    seqs = open(seq_inp).readlines()
+
+    seq_gene_starts = [i[0] for i in enumerate(seqs) if isboundary(i[1])]
+    seq_gene_starts.append(-1)
+
     pn = 0
 
     for n in ndelete:
-        for t in tl:
-            if len(t.get_edge_set()) > len(t.leaf_nodes()) + 2:
-                delete_taxa(t, n - pn)
+        outseq = open(seq_inp + "m" + "%02d" % n, 'w')
+        out = open(inp + "m" + "%02d" % n, 'w')
+        i = 0
+        for i in range(len(tl)):
+#        for t, s1, s2 in zip(tl, seq_gene_starts[:-1], seq_gene_starts[1:]):
+            s1 = seq_gene_starts[i]
+            s2 = seq_gene_starts[i+1]
+            modseq = delete_taxa(tl[i], seqs[s1:s2], n - pn)
+            outseq.write(''.join(modseq))
+
         pn = n
-        tl.write_to_path(inp + "m" + "%02d" % n, 'newick')
+        tl.write_to_stream(out, 'newick')
+        startree.write_to_stream(out, 'newick')
+        outseq.close()
+        out.close()
+        
